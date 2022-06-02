e30c95faf29749e69a9c764e2b9ec5a8494fab90
Henrique Ferreira <62857389+henferreiraofc@users.noreply.github.com>
2022-06-02 19:15:49 -0300

    finalização inicial do código

--- desafios/substituir-pares/sub-pares.js
+++ desafios/substituir-pares/sub-pares.js
@@ -1,5 +1,16 @@
 function substituiPares(array) {
-    if (!array) return "Lista vazia, adicionando -1";
-}
+    if (!array) return "Parametro indefinido, adicionando -1.";
+    if (!array.length) return "Lista vazia, retornando -1.";
 
-console.log("substituiPares(false)");
+    for (let lista = 0; lista < array.length; lista++) {
+        if (array === 0) {
+            console.log("Item já é zero"); 
+        } else if (array[lista] % 2 === 0) {
+            console.log("item é par, substituindo por 0.");
+            array[lista] = 0;
+        }
+    }
+    return array;
+}
+var arr = [2, 3, 4, 5, 9, 6]
+console.log(substituiPares(arr));
