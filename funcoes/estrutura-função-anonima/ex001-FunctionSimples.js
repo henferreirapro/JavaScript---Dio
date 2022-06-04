2ff23f154e40d2ae8b46f6f3a4ca2338aa60f92f
Henrique Ferreira <62857389+henferreiraofc@users.noreply.github.com>
2022-06-04 15:45:53 -0300

    Add exercicios e anotações sobre funções

---
+++ funcoes/estrutura-função-anonima/ex001-FunctionSimples.js
@@ -0,0 +1,6 @@
+function soma(a, b) {
+    console.log(`A soma de ${a} + ${b} é igual a ${a + b}.`);
+}
+
+soma(1, 2)
+soma(4, 6)
---
+++ funcoes/estrutura-função-anonima/ex003-FunctionAnonima.js
@@ -0,0 +1,7 @@
+const multiplicar = function (a, b) {
+    resposta = console.log(`A Multiplicação de ${a} x ${b} é ${a * b}.`);
+    return resposta;
+}
+
+multiplicar(4, 6)
+multiplicar(2, 10)
