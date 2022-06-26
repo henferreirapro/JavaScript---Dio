6de7cb60d15abfe80090de66d5f3fedbf00bafb1
Henrique Ferreira <62857389+henferreiraofc@users.noreply.github.com>
2022-06-25 21:25:08 -0300

    Pratica com Modulos
    Fiz 3 funções em um arquivo funcoes.mjs para serem exportados para o arquivo main.mjs.
    
    Funções recebem os parâmetros nome, idade, cidade e hobby.
    As funções são importadas para o HTML e executados no console do navegador recebendo os parâmetros definidos.

---
+++ 0-atividades/ex-015-modulos/main.mjs
@@ -0,0 +1,5 @@
+import { mostraIdade, mostraCidade, mostraHobby } from "./funcoes.mjs";
+
+console.log(mostraIdade("Henrique", 27));
+console.log(mostraCidade("Henrique", "São Paulo"));
+console.log(mostraHobby("Henrique", "jogar games"));
